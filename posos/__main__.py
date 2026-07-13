e72b7fecf574b63c02d2622b73d687f98c0f9fde
OG-Jaggman <ogjaggman@gmail.com>
2026-07-13 19:02:11 -0400

    Enable gift cards, payment choices, reboot, and cart fixes

--- posos/__main__.py
+++ posos/__main__.py
@@ -1,12 +1,19 @@
 from .app import main
 from .refunds import patch_sales_tab
 from .saved_orders import patch_register_saved_orders
+from .ui_fixes import patch_cart_selection
+from .giftcards import patch_gift_card_manager_tab, patch_payment_screen
+from .system_controls import patch_system_controls
 from .modern_ui import patch_modern_register_ui
 from .modern_ui_runtime import patch_modern_register_runtime
 from .modern_screens import patch_modern_secondary_screens
 
 patch_sales_tab()
 patch_register_saved_orders()
+patch_cart_selection()
+patch_gift_card_manager_tab()
+patch_payment_screen()
+patch_system_controls()
 patch_modern_register_ui()
 patch_modern_register_runtime()
 patch_modern_secondary_screens()
