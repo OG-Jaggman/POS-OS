e83f231568088b2ba6570bd70e5c326bb4118fac
OG-Jaggman <ogjaggman@gmail.com>
2026-07-13 18:36:18 -0400

    Enable modern login and manager styling

--- posos/__main__.py
+++ posos/__main__.py
@@ -3,11 +3,13 @@ from .refunds import patch_sales_tab
 from .saved_orders import patch_register_saved_orders
 from .modern_ui import patch_modern_register_ui
 from .modern_ui_runtime import patch_modern_register_runtime
+from .modern_screens import patch_modern_secondary_screens
 
 patch_sales_tab()
 patch_register_saved_orders()
 patch_modern_register_ui()
 patch_modern_register_runtime()
+patch_modern_secondary_screens()
 
 if __name__ == "__main__":
     main()
