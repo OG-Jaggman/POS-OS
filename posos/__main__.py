8aa228d87e2985b2eb3666c805d6689a502fc580
OG-Jaggman <ogjaggman@gmail.com>
2026-07-13 19:10:25 -0400

    Enable post-transaction customer receipt prompt

--- posos/__main__.py
+++ posos/__main__.py
@@ -3,6 +3,7 @@ from .refunds import patch_sales_tab
 from .saved_orders import patch_register_saved_orders
 from .ui_fixes import patch_cart_selection
 from .giftcards import patch_gift_card_manager_tab, patch_payment_screen
+from .receipt_flow import patch_post_transaction_receipts
 from .system_controls import patch_system_controls
 from .modern_ui import patch_modern_register_ui
 from .modern_ui_runtime import patch_modern_register_runtime
@@ -13,6 +14,7 @@ patch_register_saved_orders()
 patch_cart_selection()
 patch_gift_card_manager_tab()
 patch_payment_screen()
+patch_post_transaction_receipts()
 patch_system_controls()
 patch_modern_register_ui()
 patch_modern_register_runtime()
